f657dc9a64e34f39771665da1ed81325c2c64f63
João Pedro <jpedrodaibello1999@outlook.com>
2022-05-12 13:30:37 -0300

    feat: configuring Firebase with Firebase CLI

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:cuidapet_mobile_2/app/app_module.dart';
 import 'package:cuidapet_mobile_2/app/app_widget.dart';
+import 'package:cuidapet_mobile_2/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.android,
+  );
+
   runApp(
     ModularApp(
       module: AppModule(),
